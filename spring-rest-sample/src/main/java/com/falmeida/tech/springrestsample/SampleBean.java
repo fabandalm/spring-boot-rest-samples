2263d1f9fde53d8fc3ba6fdce8f55eb9456c8e85
Almeida <falmeida.tech@gmail.com>
2020-04-13 17:26:30 -0400

    Initial Commit

---
+++ spring-rest-sample/src/main/java/com/falmeida/tech/springrestsample/SampleBean.java
@@ -0,0 +1,20 @@
+package com.falmeida.tech.springrestsample;
+
+public class SampleBean {
+
+	private String message;
+
+	public SampleBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
---
+++ spring-rest-sample/src/main/java/com/falmeida/tech/springrestsample/SampleController.java
@@ -0,0 +1,27 @@
+package com.falmeida.tech.springrestsample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@RequestMapping(method=RequestMethod.GET,path="/new-sample")
+	public String newSample() {
+		return "new Sample";
+	}
+	
+	@GetMapping(path="new-sample/bean")
+	public SampleBean newSampleBean() {
+		return new SampleBean("new Sample Bean");
+	}
+	
+	@GetMapping(path="/new-sample/bean/{name}")
+	public SampleBean newSampleBeanName(@PathVariable String name) {
+		return new SampleBean(String.format("Sample Bean Name, %s",name));
+	}
+	
+}
