8b4b939ddc58275bbbae31e7af18b6b12bb401b0
Almeida <falmeida.tech@gmail.com>
2020-04-16 17:29:25 -0400

    New implementation for Internationalization.

--- spring-rest-sample/src/main/java/com/falmeida/tech/springrestsample/service/SampleController.java
+++ spring-rest-sample/src/main/java/com/falmeida/tech/springrestsample/service/SampleController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -28,10 +29,9 @@ public class SampleController {
 	}
 	
 	@GetMapping(path="new-sample/bean/i18n")
-	public String newSampleBeanInternational(@RequestHeader(name="Accept-Language",required=false)
-													Locale locale) {
+	public String newSampleBeanInternational() {
 		
-		return messageSource.getMessage("good.morning.message", null, locale);
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 		
 	}
 	
