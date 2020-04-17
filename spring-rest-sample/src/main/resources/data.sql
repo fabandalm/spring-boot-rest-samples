143e35c1f95adeed6e5584996a51b13651e3c897
Almeida <falmeida.tech@gmail.com>
2020-04-17 18:32:24 -0400

    Added person data.

--- spring-rest-sample/src/main/resources/data.sql
+++ spring-rest-sample/src/main/resources/data.sql
@@ -3,4 +3,4 @@ insert into user values (2,sysdate(),'Joshua');
 insert into user values (3,sysdate(),'Mary');
 insert into post values (101,'My First Post',1);
 insert into post values (102,'My Second Post',2);
-
+insert into person values (9991,'Joe');
