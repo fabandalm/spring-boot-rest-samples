89ae58c2dff2ddc5935d2a2b20a42c5c46f29eb7
Almeida <falmeida.tech@gmail.com>
2020-04-16 22:02:58 -0400

    Added data for the table post.

--- spring-rest-sample/src/main/resources/data.sql
+++ spring-rest-sample/src/main/resources/data.sql
@@ -1,4 +1,6 @@
 insert into user values (1,sysdate(),'John');
 insert into user values (2,sysdate(),'Joshua');
 insert into user values (3,sysdate(),'Mary');
+insert into post values (101,'My First Post',1);
+insert into post values (102,'My Second Post',2);
 
