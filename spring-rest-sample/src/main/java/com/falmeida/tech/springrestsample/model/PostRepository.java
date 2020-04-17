8f8f446c884a413002d1ddc4c82bd66ab14df1bf
Almeida <falmeida.tech@gmail.com>
2020-04-16 22:06:41 -0400

    Created a new repository for the entity post.

---
+++ spring-rest-sample/src/main/java/com/falmeida/tech/springrestsample/model/PostRepository.java
@@ -0,0 +1,7 @@
+package com.falmeida.tech.springrestsample.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Integer>{
+
+}
