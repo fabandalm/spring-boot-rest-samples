7047968bf35409bbfc196d2d39eb9bbf0e45e5c9
Almeida <falmeida.tech@gmail.com>
2020-04-20 21:03:09 -0400

    Initial commit.

---
+++ spring-tour-sample/src/main/java/com/falmeida/tech/springtoursample/SpringTourSampleApplication.java
@@ -0,0 +1,13 @@
+package com.falmeida.tech.springtoursample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTourSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTourSampleApplication.class, args);
+	}
+
+}
