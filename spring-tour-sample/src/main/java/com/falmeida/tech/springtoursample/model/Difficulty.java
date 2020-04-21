f2756d9acba05076467e6af31fe7cf47a06817f3
Almeida <falmeida.tech@gmail.com>
2020-04-20 21:25:47 -0400

    Added new entities.

---
+++ spring-tour-sample/src/main/java/com/falmeida/tech/springtoursample/model/Difficulty.java
@@ -0,0 +1,5 @@
+package com.falmeida.tech.springtoursample.model;
+
+public enum Difficulty {
+    Easy, Medium, Difficult, Varies;
+}
---
+++ spring-tour-sample/src/test/java/com/falmeida/tech/springtoursample/SpringTourSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falmeida.tech.springtoursample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTourSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
